aac25eaf11d98012e020178feb098c6b7f29ed3a
minh nguyen <imnotminh@gmail.com>
2019-03-29 23:31:04 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,7 +12,9 @@ def fourth_challenge
         attitude:"worried"
         },
         {
-        name:
+        name:"Mercutio",
+        age:"18",
+        attitude:"hot-headed"
           
         }
       ]
